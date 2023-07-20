6aeab756077d4a8f9e4a31284ea8aba453ad3fb1
Yaroslav Donchuk <donchukoff@gmail.com>
2023-07-20 17:02:51 +0300

    Removing code from blog spec

--- cypress/e2e/blog.spec.cy.js
+++ cypress/e2e/blog.spec.cy.js
@@ -4,13 +4,7 @@ describe('blog page', () => {
   it('should allow user to visit blog and article', () => {
     cy.visit('https://shopmtn.co.uk');
 
-    cy.contains('.needsclick', 'STAY ON SHOPMTN.CO.UK')
-      .click();
-
-    cy.wait(20000);
-
-    cy.contains('.needsclick', 'No thanks! I prefer to pay full price.')
-      .click();
+    
 
     cy.contains('.gr-footer__nav-link', 'BLOG')
       .click();
