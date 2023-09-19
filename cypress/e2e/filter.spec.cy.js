08c68a3363fa182e8bf42c76a26399a6fcc243ab
Yaroslav Donchuk <donchukoff@gmail.com>
2023-09-19 09:19:10 +0300

    REmoving additional code

--- cypress/e2e/filter.spec.cy.js
+++ cypress/e2e/filter.spec.cy.js
@@ -5,13 +5,7 @@ describe('Filter functionality', () => {
     cy.visit('/');
 
 
-    cy.contains('.needsclick', 'STAY ON SHOPMTN.CO.UK')
-      .click();
-
-    cy.wait(20000);
-
-    cy.contains('.needsclick', 'No thanks! I prefer to pay full price.')
-      .click();
+    
 
 
 
